8e30d3eec59ca460267a9d642441c655fdd270bc
Matt Fischer <matt.fischer@garmin.com>
2017-08-06 18:24:55 -0500

    Added gravity and non-perfect elastic collisions

--- simulation.py
+++ simulation.py
@@ -3,6 +3,9 @@ import geo
 import itertools
 import math
 
+R = 0.75
+GRAVITY = geo.Vector(0, 20)
+
 class Circle(object):
     def __init__(self, position, radius, mass, velocity):
         self.position = position
@@ -33,12 +36,12 @@ class Simulator(object):
         circle.position -= normal * distance
 
     def calculate_transferred_momentum(self, velocity1, velocity2, inverse_mass1, inverse_mass2, normal):
-        return normal * (velocity1 - velocity2) * 2 / (inverse_mass1 + inverse_mass2)
+        return normal * (velocity1 - velocity2) * (1 + R) / (inverse_mass1 + inverse_mass2)
 
     def handle_circle_circle_collision(self, circle1, circle2):
         distance = circle1.position - circle2.position
-        normal = (circle2.position - circle1.position).normalize()
-        if (circle1.position - circle2.position).magnitude2() <= (circle1.radius + circle2.radius) ** 2 and (circle2.velocity - circle1.velocity) * normal < 0: 
+        if (circle1.position - circle2.position).magnitude2() <= (circle1.radius + circle2.radius) ** 2 and (circle2.velocity - circle1.velocity) * (circle2.position - circle1.position) < 0: 
+            normal = (circle2.position - circle1.position).normalize()
             distance = circle1.radius + circle2.radius - (circle2.position - circle1.position).magnitude()
             self.stabilize_circle(circle1, normal, distance / 2)
             self.stabilize_circle(circle2, -normal, distance / 2)
@@ -58,6 +61,7 @@ class Simulator(object):
 
     def advance(self, time):
         for circle in self.circles:
+            circle.velocity += GRAVITY * time
             circle.position += circle.velocity * time   
 
         for (circle1, circle2) in itertools.combinations(self.circles, 2):
