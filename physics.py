04c40c613899df95d89c458bf69d4ffbef49acb5
Matt Fischer <matt.fischer@garmin.com>
2017-08-06 16:13:16 -0500

    Fixed drawing calculations

--- physics.py
+++ physics.py
@@ -32,15 +32,15 @@ class Window(QtGui.QMainWindow):
 
     def onCanvasPaintEvent(self, event):
         painter = QtGui.QPainter(self.ui.canvas)
-        pixel_scale = self.height() / VIEW_HEIGHT
+        pixel_scale = self.ui.canvas.height() / VIEW_HEIGHT
         pen = QtGui.QPen(QtGui.QColor(0, 0, 0), 2, Qt.SolidLine, Qt.SquareCap, Qt.MiterJoin)
         brush = QtGui.QBrush(QtGui.QColor(0xE0, 0xF0, 0xFF))
         painter.setPen(pen)
         painter.setBrush(brush)
         painter.setRenderHint(QtGui.QPainter.Antialiasing)
         for circle in self.simulator.circles:
-            x = circle.position.x * pixel_scale + self.width() / 2
-            y = circle.position.y * pixel_scale + self.height() / 2
+            x = circle.position.x * pixel_scale + self.ui.canvas.width() / 2
+            y = circle.position.y * pixel_scale + self.ui.canvas.height() / 2
             radius = circle.radius * pixel_scale
             painter.drawEllipse(QtCore.QPoint(x, y), radius, radius)
 
@@ -48,7 +48,7 @@ class Window(QtGui.QMainWindow):
         self.setSimulatorBounds()
 
     def setSimulatorBounds(self):
-        unit_scale = VIEW_HEIGHT / self.height()
+        unit_scale = VIEW_HEIGHT / self.ui.canvas.height()
         min_x = -self.ui.canvas.width() * unit_scale / 2
         min_y = -VIEW_HEIGHT / 2
         max_x = -min_x
